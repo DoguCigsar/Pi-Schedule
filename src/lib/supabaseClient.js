d4f1a511e150a4ccad9abfe3ee6e2b112b79273d
DoguCigsar <dogu.cigsar@cos-soft.net>
2024-10-21 21:07:05 -0400

    env fix

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
-import { env } from '$env/dynamic/public';
-export const supabase = createClient(env.PUBLIC_URL, env.PUBLIC_CLIENT_SECRET);
+import { PUBLIC_URL, PUBLIC_CLIENT_SECRET } from '$env/static/public';
+export const supabase = createClient(PUBLIC_URL, PUBLIC_CLIENT_SECRET);
