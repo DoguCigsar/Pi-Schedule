553d6ca8f73b82afee57f8fd0f44fa089d923a6f
DoguCigsar <dogu.cigsar@cos-soft.net>
2024-11-01 18:46:02 +0000

    Notification available in all pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
+<script>
+	import NotificationSystem from '$lib/NotificationSystem.svelte';
+</script>
+
 <svelte:head>
 	<title>𝝅 Schedule</title>
 </svelte:head>
 
 <slot />
+
+<NotificationSystem />
 
 <h4>© 2024 Dogu Cigsar/COS-Soft</h4>
 <h5><a href="/privacy_policy">Privacy Policy</a></h5>
